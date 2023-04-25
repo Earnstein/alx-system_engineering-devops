1015a0981191f297abc14f6805b53a36bfa15fd0
Bakare <bdamilola00@gmail.com>
2023-04-25 14:13:30 -0700

    all task done

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""script using this REST API, for a given employee ID,
-returns information about his/her TODO list progress."""
-import requests as r
+"""Returns todo list information for a given employee ID."""
+import requests
 import sys
-
 if __name__ == '__main__':
     url = 'https://jsonplaceholder.typicode.com/'
     usr_id = r.get(url + 'users/{}'.format(sys.argv[1])).json()
@@ -16,3 +14,4 @@ if __name__ == '__main__':
                                                           len(completed),
                                                           len(to_do)))
     [print("\t {}".format(title)) for title in completed]
+
